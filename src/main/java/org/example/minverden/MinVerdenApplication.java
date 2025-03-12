9ae3a8d31f512fd76acbeb9fa77dcd3484b5f70e
shuresm57 <vsorensen31@gmail.com>
2025-03-12 11:13:09 +0100

    first commit

---
+++ src/main/java/org/example/minverden/MinVerdenApplication.java
@@ -0,0 +1,14 @@
+package org.example.minverden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class MinVerdenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinVerdenApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/minverden/controller/HomeController.java
@@ -0,0 +1,23 @@
+package org.example.minverden.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "/index";
+    }
+
+    @GetMapping("/niko")
+    public String niko(){
+        return "/home/niko";
+    }
+
+    @GetMapping("/home/valde")
+    public String valde(){
+        return "/home/valde";
+    }
+}
